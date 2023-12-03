837708323234ac8c4e4633e29780064e78809ae2
Valentin Golev <v.golev@gmail.com>
2023-12-03 18:00:40 +0100

    ok

--- src/conf.rs
+++ src/conf.rs
@@ -23,9 +23,8 @@ impl Plugin for ConfigPlugin {
 pub struct Configuration {
     // #[inspector(min = 0.0, max = 2.0)]
     // pub track_point_scale: f32,
-
-    // #[inspector(min = 0.0, max = 2000.0)]
-    // pub track_point_min_distance: f32,
+    #[inspector(min = 0.0, max = 2000.0)]
+    pub camera_scale: f32,
 }
 
 fn on_modify_configuration(
@@ -45,18 +44,20 @@ fn on_modify_configuration(
 
 fn setup(mut commands: Commands) {
     // let config_dir = dirs::config_dir().unwrap().join("your-amazing-game");
-    let config_dir = Path::new("/Users/valentin/Work/cw/jamtrain");
+    //todo
+    let config_dir = Path::new("/Users/valentin/Work/cw/trashure");
 
     let pers = Persistent::<Configuration>::builder()
         .name("configuration")
         .format(StorageFormat::Json)
         .path(config_dir.join("configuration.json"))
         .default(Configuration {
+            camera_scale: 2.5,
             // track_point_scale: 1.0,
             // track_point_min_distance: 2.0,
         })
         .build()
-        .expect("failed to initialize key bindings");
+        .expect("failed to initialize ");
 
     commands.insert_resource(pers.get().clone());
     commands.insert_resource(pers);
